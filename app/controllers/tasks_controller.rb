f2fa485cfbdf39da6d3e4cf48f7f0df45c95ad08
kotaro.sugita <shake1574@gmail.com>
2020-05-28 09:43:10 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,14 +12,14 @@ class TasksController < ApplicationController
     end
     
     def create
-      @task = Task.new(task_params)
-      
-      if @task.save
-        flash[:success] = 'Task が正常に投稿されました'
-        redirect_to @task
-      else
-        flash.now[:danger] = 'Task が投稿されませんでした'
-        render :new
+      @task = current_user.tasks.build(task_params)
+    if @task.save
+      flash[:success] = 'メッセージを投稿しました。'
+      redirect_to root_url
+    else
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+      flash.now[:danger] = 'メッセージの投稿に失敗しました。'
+      render 'toppages/index'
       end
     end
     
